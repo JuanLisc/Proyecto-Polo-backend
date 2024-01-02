f68440cbd42c8ad58f2b2cdf6242ca30ff6a9503
Juan <JLische-@hotmail.com>
2024-01-01 21:25:32 -0300

    [FIX]

--- src/sequelize.ts
+++ src/sequelize.ts
@@ -4,7 +4,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const sequelize = new Sequelize(process.env.DB_NAME!, process.env.DB_USERNAME!, process.env.DB_PASSWORD, {
-	host: 'localhost',
+	host: process.env.DB_HOST,
 	dialect: 'mysql',
 	define: {
 		freezeTableName: true
